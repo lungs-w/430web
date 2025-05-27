e9738896b0c373d6ec0ff11093786a4a6309512c
lungpages <79396549+lungs-w@users.noreply.github.com>
2025-05-27 15:59:13 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function enterSite() {
-    // 简单模拟确认后进入主页
-    window.location.href = "main.html";
+    function checkAnswers() {
+        const a1 = document.getElementById("q1").value.trim().toLowerCase();
+        const a2 = document.getElementById("q2").value.trim();
+        const a3 = document.getElementById("q3").value.trim().toLowerCase();
+      
+        if (a1 === "1999" && a2 === "Tsrika" && (a3 === "176" || a3 === "153")) {
+          // 正确就跳转
+          window.location.href = "main.html";
+        } else {
+          // 错误就提示
+          document.getElementById("result").textContent = "答案不正确，请再试一次！";
+        }
+      }
+      
   }
   
