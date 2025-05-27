aa79c710023ca60c73880b1d943b05cfd6699b5a
lungpages <79396549+lungs-w@users.noreply.github.com>
2025-05-27 16:12:07 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-function enterSite() {
-    function checkAnswers() {
-        const a1 = document.getElementById("q1").value.trim().toLowerCase();
-        const a2 = document.getElementById("q2").value.trim();
-        const a3 = document.getElementById("q3").value.trim().toLowerCase();
-      
-        if (a1 === "1999" && a2 === "Tsrika" && (a3 === "176" || a3 === "153")) {
-          // 正确就跳转
-          window.location.href = "main.html";
-        } else {
-          // 错误就提示
-          document.getElementById("result").textContent = "答案不正确，请再试一次！";
-        }
-      }
-      
-  }
+function checkAnswers() {
+    const a1 = document.getElementById("q1").value.trim().toLowerCase();
+    const a2 = document.getElementById("q2").value.trim().toLowerCase();
+    const a3 = document.getElementById("q3").value.trim().toLowerCase();
   
+    if (a1 === "1999" && a2 === "tsrika" && (a3 === "176" || a3 === "153")) {
+      // 正确就跳转
+      window.location.href = "main.html";
+    } else {
+      // 错误就提示
+      document.getElementById("result").textContent = "答案不正确，请再试一次！";
+    }
+  }
